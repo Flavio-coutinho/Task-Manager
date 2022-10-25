ff387425861109d0ae9c6e9387d1d40606889832
Flavio Coutinho <coutinhoflavio20@gmail.com>
2022-10-25 12:22:44 -0300

    2/6

---
+++ TaskManager/TaskManager/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManager.Controllers
+{
+    [ApiController]
+    [Route("[api/controller]")]
+    public class LoginController : ControllerBase
+    {
+       private readonly ILogger<LoginController> _logger;
+       
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult EfectuarLogin([FromBody] )
+        {
+
+        }
+    }
+}
